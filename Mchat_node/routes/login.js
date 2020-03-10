f1f4741a42882778b065658ffbe47252d1f9e902
zhuzhijian <670352956@qq.com>
2020-03-10 17:01:37 +0800

    login的api

--- Mchat_node/routes/login.js
+++ Mchat_node/routes/login.js
@@ -5,21 +5,27 @@ var user = require('../db/user');
 //导入mysql的基本连接
 var mysql = require('../db/basicConnection');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  let query = req.query;
-  let index = [];
-  let value = [];
-  let data = 1;
-  for(var key in query){
-    index.push(key);
-    value.push(query[key]);
-  }
+/* GET token listing. */
+router.post('/login', function(req, res, next) {
+  let index = ['account', 'password'];
+  let value = [req.body.account, req.body.password]; // post专用
+  console.log(req.cookies)
   let sql = user.select(index,value);
   mysql.query(sql,(args,feild) => {
-    data = args;
-    res.send({data});
+    if(args.length > 0) {
+      const response = {};
+      response.data = args;
+      response.code = 0;
+      response.message = '登录成功'
+      res.send(response);
+    }else{
+      res.send({code: -1, message: '登录失败'})
+    }
   });
 });
+
+router.get('/info', function(req, res, next) {
+  const token = req.Authorization
+})
 
 module.exports = router;
