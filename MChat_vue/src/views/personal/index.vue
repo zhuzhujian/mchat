1f0acfd353f689cdeaef1896943c68e4182b9b8c
zhuzhijian <670352956@qq.com>
2020-03-30 08:56:22 +0800

    update personal main

--- MChat_vue/src/views/personal/index.vue
+++ MChat_vue/src/views/personal/index.vue
@@ -2,49 +2,68 @@
   <div class="person-container">
     <div class="friend-list-contanier">
       <el-card class="friend-list" shadow="always">
-        <div slot="header" style="display: inline-block; text-align: center; width: 100%; font-size: 16px"><div>{{list === 'friend' ? '我的好友' : '我的群聊'}}</div></div>
-        <div v-if="list === 'friend'" class="friend-list-center">
-          <div>你好友列表为空，请先添加</div>
-        </div>
-        <div v-if="list === 'group'" class="friend-list-center">
-          <div>
-            你暂时未添加群聊，请先添加
-          </div>
+        <div class="friend-list-body">
+          <router-view></router-view>
         </div>
         <div class="friend-list-footer">
-          <div :class="{active: list === 'friend'}">
-            <i class="el-icon-s-custom" @click="getFriend"></i>
-            <p>好友</p>
-          </div>
-          <div :class="{active: list === 'group'}">
-            <i class="el-icon-s-grid" @click="getGroup"></i>
-            <p>群聊</p>
-          </div>
+          <ul>
+            <li v-for="n in nav" :key="n.id" :class="{active: $route.path.indexOf(n.link) > -1}">
+              <router-link :to="{path: n.link}">
+                <i :class="n.class"></i>
+                <p>{{n.name}}</p>
+              </router-link>
+            </li>
+          </ul>
         </div>
       </el-card>
     </div>
     <el-card class="chat-room" shadow="always">
-      chat-room
+      <chat-room></chat-room>
     </el-card>
   </div>
 </template>
 
 <script>
+import chatRoom from '@/views/components/chatRoom'
 export default {
   name: 'personMain',
+  components: {chatRoom},
   data () {
     return {
+      nav: [{
+        name: '好友',
+        class: 'el-icon-user-solid',
+        id: 2,
+        link: '/main/personMain/friendly/own'
+      },
+      {
+        name: '群聊',
+        class: 'el-icon-s-grid',
+        id: 3,
+        link: '/main/personMain/group'
+      }],
       list: 'friend',
       friendList: [],
       groupList: []
     }
   },
+  mounted () {
+  },
   methods: {
     getFriend () {
       this.list = 'friend'
+      this.$scoket.close()
     },
     getGroup () {
       this.list = 'group'
+    }
+  },
+  sockets: {
+    connect: function (data) {
+      console.log('websocket connection')
+    },
+    test (data) {
+      console.log(data + 'test')
     }
   }
 }
@@ -59,28 +78,51 @@ export default {
     width: 300px;
     height: 100%;
     >>>.el-card__body{
-      padding: 0!important;
-    }
-    .friend-list-center{
-      height: 550px;
-      width: 100%;
-    }
-    .friend-list-footer{
-      height: 50px;
-      width: 100%;
-      display: flex;
-      .active{
-        color: #0099ff;
+      height: 100%;
+      padding: 0;
+      .friend-list-body{
+        height: calc(100% - 80px);
       }
-      div{
-        width: 50%;
-        text-align: center;
-        i{
-          font-size: 25px;
+      .friend-list-footer{
+        height: 50px;
+        width: 100%;
+        border-top: 1px solid #ebebeb;
+        ul {
+          display: flex;
+          list-style: none;
+          width: 100%;
+          justify-content: center;
+          align-items: center;
+          box-sizing: border-box;
+          li{
+            list-style: none;
+            width: 33.3%;
+            font-size: 12px;
+            a{
+              color: #000;
+              text-decoration: none;
+              i{
+                text-decoration: none;
+                font-size: 22px;
+              }
+            }
+          }
+          .active{
+            a{
+              color: #0099ff;
+            }
+          }
         }
-        p{
-          font-size: 14px;
-          margin: 5px 0;
+        div{
+          width: 50%;
+          text-align: center;
+          i{
+            font-size: 25px;
+          }
+          p{
+            font-size: 14px;
+            margin: 5px 0;
+          }
         }
       }
     }
@@ -89,6 +131,10 @@ export default {
     flex: 1;
     height: 100%;
     margin-left: 15px;
+    >>>.el-card__body{
+      padding: 0;
+      height: 100%;
+    }
   }
 }
 </style>
