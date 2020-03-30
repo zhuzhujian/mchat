c5e89aa7dae8ca7a2a41f3cd88612907449b19ec
zhuzhijian <670352956@qq.com>
2020-03-30 08:47:22 +0800

    group

---
+++ MChat_vue/src/views/group/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="group-contanier">group list</div>
+</template>
+
+<script>
+export default {
+  name: 'group',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
