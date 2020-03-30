55099e0326df7a1753eb836c888326ade801e4e9
zhuzhijian <670352956@qq.com>
2020-03-30 08:38:50 +0800

    添加全局nodate组件

---
+++ MChat_vue/src/libs/index.js
@@ -0,0 +1,8 @@
+import noData from './nodate/noDate.vue'
+const install = {
+  install: function (Vue) {
+    Vue.component('vNodata', noData)
+  }
+}
+
+export default install
---
+++ MChat_vue/src/libs/nodate/noDate.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="vchat-no-data">
+        <div class="no-data-bg" :style="{backgroundImage: 'url(' + src + ')'}"></div>
+        <slot></slot>
+    </div>
+</template>
+<script>
+import bg from '../../assets/images/zwsj1.png'
+export default{
+  props: ['url'],
+  name: 'vchatNodata',
+  data () {
+    return {}
+  },
+  computed: {
+    src () {
+      return this.url || bg
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+    .vchat-no-data{
+        width:100%;
+        margin-bottom: 10px;
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        align-items: center;
+        .no-data-bg{
+            width:200px;
+            height: 200px;
+            background-repeat: no-repeat;
+            margin-bottom: 10px;
+            background-size: contain;
+        }
+    }
+</style>
